e099c82abf3b779c6043afdadf9f6160afee26be
richieshi <richie_shi@hotmail.com>
2014-02-06 22:15:33 -0500

    Comments and changes for state variables

--- src/com/team1815/robot/State.java
+++ src/com/team1815/robot/State.java
@@ -2,8 +2,9 @@ package com.team1815.robot;
 
 public class State {
     final static int NORMAL = 0;
-    final static int NOT_SHOT = 0;
     final static int GO_FETCH = 1;
-    final static int GO_FORWARD = 1;
-    final static int SHOT = 1;
+    
+    final static int NOT_SHOT = 2; //ball has not been shot in autonomous mode
+    final static int GO_FORWARD = 3; //robot is moving forward in autonomous mode
+    final static int SHOT = 4; //ball has been shot already in autonomous mode
 }
