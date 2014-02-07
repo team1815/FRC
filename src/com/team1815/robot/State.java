f3d95b87db31372460218022a0fb64eca58bfb9f
Eric Ye <ericye16@gmail.com>
2014-02-06 22:20:47 -0500

    commented states

--- src/com/team1815/robot/State.java
+++ src/com/team1815/robot/State.java
@@ -1,8 +1,8 @@
 package com.team1815.robot;
 
 public class State {
-    final static int NORMAL = 0;
-    final static int GO_FETCH = 1;
+    final static int NORMAL = 0; //normal human driving in teleop mode
+    final static int GO_FETCH = 1; //robot chases after ball autonomously in teleop mode
     
     final static int NOT_SHOT = 2; //ball has not been shot in autonomous mode
     final static int GO_FORWARD = 3; //robot is moving forward in autonomous mode
