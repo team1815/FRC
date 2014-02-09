5101a80f16fa07603106f195d80a0e0c9b250fcf
Eric Ye <ericye16@gmail.com>
2014-02-09 18:10:00 -0500

    using timerdelay instead of threadsleep

--- src/com/team1815/robot/AutonomousMove.java
+++ src/com/team1815/robot/AutonomousMove.java
@@ -1,25 +1,13 @@
 package com.team1815.robot;
 
+import edu.wpi.first.wpilibj.Timer;
+
 public class AutonomousMove extends Thread {
-    
-    double startTime = System.currentTimeMillis();
 
     public void run() {
-        if (startTime > System.currentTimeMillis()) {
-            startTime = System.currentTimeMillis();
-        }
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException ex) {
-            ex.printStackTrace();
-        }
-        IterativeBeast1815.autonomousMove = State.NORMAL;
-    }
-    
-    public AutonomousMove() {
         IterativeBeast1815.autonomousMove = State.GO_FORWARD;
         IterativeBeast1815.autonomousShoot = State.NOT_SHOT;
-        startTime = System.currentTimeMillis();
-    }
-    
+        Timer.delay(1.0);
+        IterativeBeast1815.autonomousMove = State.NORMAL;
+    }    
 }
